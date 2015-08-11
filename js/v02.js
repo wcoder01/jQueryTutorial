3e52440a034e551c612b201fc4b9f6dc10f93e33
wcoder01 <wbenfatto@Williams-MacBook-Air.local>
2015-08-10 22:58:19 -0400

    10-aug-2015

---
+++ js/v02.js
@@ -0,0 +1,19 @@
+$(window).load(function(){ 
+		$('#div1').html("Height = " + 
+			$('#img1').height() + " / Width = " + 
+			$('#img1').width()
+			);
+
+
+	});
+
+
+
+/*
+Depois de toda a pagina carregada
+$(window).load(function(){ 
+
+Depois do DOM carregado, antes das imagens
+$(document).ready(function(){ 
+
+*/
